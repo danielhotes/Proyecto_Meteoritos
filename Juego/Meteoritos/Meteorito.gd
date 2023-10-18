6cf317c065b150a657909b559eed9d9028aa3a1f
danielhotes <danielhotes@gmail.com>
2023-10-18 10:58:19 -0300

    Ajuste en movimiento de meteoritos, fondo

--- Juego/Meteoritos/Meteorito.gd
+++ Juego/Meteoritos/Meteorito.gd
@@ -2,7 +2,7 @@ class_name Meteorito
 extends RigidBody2D
 
 ## Atributos Export
-export var vel_lineal_base:Vector2 = Vector2(200.0, 200.0)
+export var vel_lineal_base:Vector2 = Vector2(100.0, 100.0)
 export var vel_ang_base:float = 3.0
 export var hitpoints_base:float = 10.0
 
@@ -29,8 +29,8 @@ func crear(pos:Vector2, dir:Vector2, tamanio:float) -> void:
 	forma_colision.radius = radio
 	$CollisionShape2D.shape = forma_colision
 	# Calcular velocidades
-	linear_velocity = vel_lineal_base * dir / tamanio
-	angular_velocity = vel_ang_base / tamanio
+	linear_velocity = (vel_lineal_base * dir / tamanio) * aleatorizar_velocidad()
+	angular_velocity = (vel_ang_base / tamanio) * aleatorizar_velocidad()
 	# Calcular hitpoints
 	hitpoints = hitpoints_base * tamanio
 	# Solo debug
@@ -48,3 +48,8 @@ func destruir() -> void:
 	$CollisionShape2D.set_deferred("disabled", true)
 	Eventos.emit_signal("meteorito_destruido", global_position)
 	queue_free()
+
+func aleatorizar_velocidad() -> float:
+	randomize()
+	return rand_range(1.1, 1.4) 
+	
--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -124,3 +124,8 @@ func recibir_danio(danio:float) -> void:
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if anim_name == "spawn":
 		controlador_estados(ESTADO.VIVO)
+
+func _on_body_entered(body: Node) -> void:
+	if body is Meteorito:
+		destruir()
+		body.destruir()
